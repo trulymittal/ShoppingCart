3d622f1a8f504e7a8ca6a6464c1e49b997b25892
Truly Mittal <trulymittal@gmail.com>
2020-08-07 20:34:15 +0530

    checkpoint, added cart model, cart layout, and cart fragment layout

---
+++ app/src/main/java/com/example/shoppingcart/models/CartItem.java
@@ -0,0 +1,48 @@
+package com.example.shoppingcart.models;
+
+import java.util.Objects;
+
+class CartItem {
+
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "CartItem{" +
+                "product=" + product +
+                ", quantity=" + quantity +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CartItem cartItem = (CartItem) o;
+        return getQuantity() == cartItem.getQuantity() &&
+                getProduct().equals(cartItem.getProduct());
+    }
+
+}
